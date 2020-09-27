f64e48dc9de5b003d5b5eb70ec202ed6a30cf485
Balaji Saravanan <balaji.saravanan@zohocorp.com>
2020-09-27 23:51:11 +0530

    Modified client side for Thirukkural addition

--- client/src/components/APIInfo.vue
+++ client/src/components/APIInfo.vue
@@ -1,63 +1,75 @@
 <template>
-  <div class="info">
-	<ul v-for="(item, index) in api" :key="index">
-		<li>
-			<a :href="item.href" target="_blank">{{item.display}}</a>
-		</li>
-	</ul>
-		<div class="info-footer">
-			<p>
-				Are you ready to play with these thousands of mocked data. Hit the <a href="https://twobytwelve.blogspot.com/2020/05/mocked-api-documentation.html">documentation</a> page, and get started
-			</p>
-		</div>
-	</div>
+<div class="info">
+    <ul v-for="(item, index) in api" :key="index">
+        <li>
+            <a :href="item.href" target="_blank">{{item.display}}</a>
+        </li>
+    </ul>
+    <div class="info-footer">
+        <p>
+            Are you ready to play with these thousands of mocked data. Hit the <a href="https://twobytwelve.blogspot.com/2020/05/mocked-api-documentation.html">documentation</a> page, and get started
+        </p>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-  data() {
-		return {
-			api: [{
-				href: 'api/personalinfo',
-				display: 'Personal Information API'
-			},
-			{
-				href: 'api/movies',
-				display: 'Movie data API'
-			},
-			{
-				href: 'api/stockmarket',
-				display: 'Stock market Information API'
-			}]
-			
-		}
-	},
+    data() {
+        return {
+            api: [{
+                    href: 'api/personalinfo',
+                    display: 'Personal Information API'
+                },
+                {
+                    href: 'api/movies',
+                    display: 'Movie data API'
+                },
+                {
+                    href: 'api/stockmarket',
+                    display: 'Stock market Information API'
+                },
+                {
+                    href: 'api/thirukkural',
+                    display: 'The collection of poems from the classic Tamil Literature Thirukkural'
+                },
+                {
+                    href: 'api/thirukkuralDetails',
+                    display: 'The details over the poems of Thirukkural'
+                }
+            ]
+
+        }
+    },
 }
 </script>
 
 <style scoped>
 .info {
-	width: auto;
-	display: flex;
-	justify-content: center;
-	text-align: center;
-	flex-direction: column;
-	flex-wrap: wrap;
-	margin: 15rem 0 0 0;
+    width: auto;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    flex-direction: column;
+    flex-wrap: wrap;
+    margin: 15rem 0 0 0;
 }
+
 .info a {
-	text-decoration: underline;
-	color: black;
-	font-size: 2rem;
-	text-align: center;
+    text-decoration: underline;
+    color: black;
+    font-size: 2rem;
+    text-align: center;
 }
+
 .info li {
-	list-style-position: inside;
+    list-style-position: inside;
 }
+
 .info-footer {
-	padding: 3rem;
-	padding-top: 5rem;
-	font-size: 2rem;
-	font-family: Avenir, Helvetica, Arial, sans-serif;
+    padding: 3rem;
+    padding-top: 5rem;
+    font-size: 2rem;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 </style>
